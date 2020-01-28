00d79aeb422a4d48f6a65c4ce3f0d4fe4fa4ad5e
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2020-01-28 09:34:19 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -25,6 +25,7 @@ def startOver():
 @app.route('/page1')
 def renderPage1():
   if "question_2" in session:
+    print("hello")
     return redirect('/page2')
   return render_template('page1.html')
 
