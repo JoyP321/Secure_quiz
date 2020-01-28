1590abd5ce504bddbe0dcef312a9d316a05c05c6
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2020-01-28 09:51:20 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -38,7 +38,8 @@ def renderPage2():
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
     #TODO: save the favorite color in the session
-    session["question_2"]= request.form["question2"]
+    if "question_1" not in session:
+      session["question_2"]= request.form["question2"]
     return render_template('page3.html')
     
 if __name__=="__main__":
